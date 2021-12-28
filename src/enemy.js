8525546e1cedc8309f582a4b82cd622b112d2cf1
Isaac <mcauleyisaac@gmail.com>
2021-12-27 21:58:20 -0700

    Inheriting entity for enemy class

--- src/enemy.js
+++ src/enemy.js
@@ -1,8 +1,11 @@
-class Enemy {
+class Enemy extends Entity {
+    static get WIDTH() {return 3};
+    static get HEIGHT() {return 3};
+
     constructor(position) {
-        this.position = position;
+        super(position);
         this.velocity = vec2(0,0);
-        this.size = vec2(3,3);
+        this.size = vec2(Enemy.WIDTH,Enemy.HEIGHT);
         this.color = new Color(1,1,.2);
     }
 
@@ -13,5 +16,12 @@ class Enemy {
     update(context) {
         this.velocity = context.ship.position.subtract(this.position).scale(.01);
         this.position = this.position.add(this.velocity);
+    }
+
+    getBoundingBox() {
+        return {
+            width: vec2(this.position.x - Enemy.WIDTH/2, this.position.x + Enemy.WIDTH/2),
+            height: vec2(this.position.y - Enemy.HEIGHT/2, this.position.y + Enemy.HEIGHT/2),
+        }
     }
 }
