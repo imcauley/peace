8b1e293964ee0ec38b056d55d42675ea74380c2f
Isaac <mcauleyisaac@gmail.com>
2022-01-12 21:35:41 -0700

    ship collisions

--- src/ship.js
+++ src/ship.js
@@ -1,6 +1,10 @@
-class Ship {
+class Ship extends Entity {
     constructor() {
-        this.position = vec2(5,5);
+        super(vec2(5,5))
+        // this.position = ;
+        this.width = 3;
+        this.height = 3;
+
         this.velocity = vec2(0,0);
         this.size = vec2(3,3);
         this.color = new Color(.2,1,1);
@@ -10,8 +14,17 @@ class Ship {
         drawRect(this.position, this.size, this.color, 0, 0);
     }
 
+    getBoundingBox() {
+        return {
+            left: this.position.x - this.width/2,
+            right: this.position.x + this.width/2,
+            top: this.position.y + this.height/2,
+            bottom: this.position.y - this.height/2,
+        }
+    }
+
     update(context) {
         this.velocity = context.gameStick;
-        this.position = this.position.add(this.velocity);
+        this.move(this.position.copy().add(this.velocity), context);
     }
 }
