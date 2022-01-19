71e88f929b97a3a3cd16998e1a69d717c89a11bb
Isaac <mcauleyisaac@gmail.com>
2022-01-18 19:10:36 -0700

    Bounce back collisions for enemies

--- src/enemy.js
+++ src/enemy.js
@@ -17,8 +17,44 @@ class Enemy extends Entity {
         drawRect(this.position, this.size, this.color, 0, 0);
     }
 
-    handleCollision(collidedEntity) {
+    handleCollision(collision) {
+        let myBounds = this.getBoundingBox();
+        let bounds = collision.entity.getBoundingBox();
+
+        let amountInTop = myBounds.top - bounds.top;
+        let amountInBottom = bounds.bottom -  myBounds.bottom;
+        let amountInLeft =  bounds.left - myBounds.left;
+        let amountInRight = myBounds.right - bounds.right;
+
+        // console.log(`${amountInBottom} and ${amountInLeft}`)
+
+        // bounce off bottom
+        if(myBounds.bottom < bounds.top && myBounds.bottom > bounds.bottom) {
+            if(amountInBottom < amountInLeft && amountInBottom < amountInRight) {
+                this.position.y = bounds.top + Math.ceil(this.height / 2);
+            }
+        }
+
+        // bounce off top
+        if(myBounds.top > bounds.bottom && myBounds.top < bounds.top) {
+            if(amountInTop < amountInLeft && amountInTop < amountInRight) {
+                this.position.y = bounds.bottom - Math.ceil(this.height / 2);
+            }
+        }
         
+        // bounce off left
+        if(myBounds.left < bounds.right && myBounds.left > bounds.left) {
+            if(amountInLeft < amountInBottom && amountInLeft < amountInTop) {
+                this.position.x = bounds.right + Math.ceil(this.width / 2);
+            }
+        }
+
+        // bounce off right
+        if(myBounds.right > bounds.left && myBounds.right < bounds.right) {
+            if(amountInRight < amountInBottom && amountInRight < amountInTop) {
+                this.position.x = bounds.left - Math.ceil(this.width / 2);
+            }
+        }
     }
 
     getBoundingBox() {
