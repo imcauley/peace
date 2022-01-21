dabac1d1aaa4959827d9e0ae405cfb5e8e2afbd0
Isaac <mcauleyisaac@gmail.com>
2022-01-21 10:22:20 -0700

    refactored hitbox

--- src/hitbox.js
+++ src/hitbox.js
@@ -8,48 +8,65 @@ class Hitbox {
     generateEdges() {
         this.edges = [];
         for(let i = 0; i < this.points.length - 1; i++) {
-            this.edges.push([
+            this.edges.push(this.generateEdgeFromPoints(
                 this.points[i],
                 this.point[i+1]
-            ])
+            ))
         }
-        this.edges.push([
+        this.edges.push(this.generateEdgeFromPoints(
             this.points[0],
             this.point[this.points.length - 1]
-        ])
+        ))
     }
 
-    generateEquations() {
-        this.equations = [];
-        this.edges.forEach(edge => {
-            let slope = (edge[0].y - edge[1].y) / (edge[0].x - edge[1].x);
-            let intercept = edge[0].y - (slope * edge[0].x);
+    generateEdgeFromPoints(p1, p2) {
+        return {
+            points: [p1,p2],
+            equation: this.generateEquationFromPoints(p1,p2),
+            midpoint: this.generateMidpointFromPoints(p1,p2)
+        }
+    }
 
-            let centerSide = (slope * this.center.x) + intercept - this.center.y;
+    generateEquationFromPoints(p1, p2) {
+        let slope = (p1.y - p2.y) / (p1.x - p2.x);
+        let intercept = p1.y - (slope * p1.x);
 
-            this.equations.push({
-                slope: slope,
-                intercept: intercept,
-                centerSide: centerSide
-            })
-        });
+        let centerSide = (slope * this.center.x) + intercept - this.center.y;
+
+        return {
+            slope: slope,
+            intercept: intercept,
+            centerSide: centerSide,
+        };
+    }
+
+    generateMidpointFromPoints(p1, p2) {
+        return p1.add(p2).divide(2);
     }
 
     pointInHitbox(point) {
-        for(edge of this.equations) {
+        let MAX_VALUE = 100000000;
+        let smallestDistance = MAX_VALUE;
+        let closestEdge = null;
+
+        for(edge of this.edges) {
             let side = (
-                (edge.slope * point.x) 
-                + edge.intercept 
+                (edge.equation.slope * point.x) 
+                + edge.equation.intercept 
                 - point.y
             );
 
-            let sameSide = (edge.centerSide * side) > 0;
-            
-            if(!sameSide) {
-                return false;
+            let sameSide = (edge.equation.centerSide * side) > 0;
+
+            if(sameSide) {
+                let distanceToEdge = edge.midpoint.distance(point);
+                if(smallestDistance > distanceToEdge) {
+                    smallestDistance = distanceToEdge;
+                    closestEdge = edge;
+                }
             }
         }
 
-        return true;
+        return closestEdge;
     }
 }
